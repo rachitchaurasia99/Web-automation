2f4549de8767719852ea3eadf74085ac5955e33a
Ratchet-prog <62827625+Ratchet-prog@users.noreply.github.com>
2022-07-20 12:25:19 +0530

    Update form.rb

--- exercise5/features/pages/form.rb
+++ exercise5/features/pages/form.rb
@@ -6,7 +6,7 @@ class Form
      text_field(:city, xpath: '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div/div[1]/input')
      text_field(:college, xpath: '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[5]/div/div/div[2]/div/div[1]/div/div[1]/input')
      text_field(:branch, xpath: '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[6]/div/div/div[2]/div/div[1]/div/div[1]/input')
-     div(:search, xpath: '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
+     div(:submit, xpath: '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
      div(:male, xpath: '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div[1]/div/span/div/div[1]')
      div(:female, xpath: '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div[1]/div/span/div/div[2]')
      def visit
@@ -25,7 +25,7 @@ class Form
       
       end
     def submit
-      search_element.click
+      submit_element.click
     end
      def close
        @browser.close
